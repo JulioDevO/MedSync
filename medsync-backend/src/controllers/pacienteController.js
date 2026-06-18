e4f849fab34f226c4085c46635e10ad311da89c2
JulioDevO <julio.santos.developer@gmail.com>
2026-06-18 09:33:41 -0300

    feat: cria rota GET e controller para listagem de pacientes

--- medsync-backend/src/controllers/pacienteController.js
+++ medsync-backend/src/controllers/pacienteController.js
@@ -1,5 +1,6 @@
 const db = require("../database/db");
 
+//Função 1: Cadastrar (POST)
 exports.cadastrarPaciente = async (req, res) => {
   const { nome, cpf, telefone, convenio } = req.body;
 
@@ -20,6 +21,21 @@ exports.cadastrarPaciente = async (req, res) => {
   } catch (err) {
     return res.status(400).json({
       erro: "Erro ao cadastrar. Verifique se o CPF já existe.",
+      detalhes: err.message,
+    });
+  }
+};
+
+//Função 2: Listar (GET)
+exports.listarPacientes = async (req, res) => {
+  const sql = "SELECT * FROM pacientes";
+
+  try {
+    const [pacientes] = await db.query(sql);
+    return res.status(200).json(pacientes);
+  } catch (err) {
+    return res.status(500).json({
+      erro: "Erro ao buscar pacientes no banco de dados.",
       detalhes: err.message,
     });
   }
--- medsync-backend/src/routes/pacienteRoutes.js
+++ medsync-backend/src/routes/pacienteRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const PacienteController = require("../controllers/pacienteController");
 
 router.post("/", PacienteController.cadastrarPaciente);
+router.get("/", PacienteController.listarPacientes);
 
 module.exports = router;
